c7e2fad80427d07d74064ef72d260953c883efa7
AlleSilvaa <126684613+AlleSilvaa@users.noreply.github.com>
2023-05-03 12:10:34 -0300

    atualizado

--- js/script.js
+++ js/script.js
@@ -10,5 +10,37 @@
  
  //Atrelando um evento de click ao button que foi recuperado.
  btn.addEventListener("click" , function(){
-    console.log(btn.innerHTML);
- } );
+
+    //Função Matemática - Math
+    //randon = gera números aleatórios enre 0 e 1. Ex: 0.232323;
+    //floor  = arredonda o número para baixo.
+    //ceil   = arredonda o número para cima.
+    //round  = arredonda o número aleatóriamente.
+    let r,g,b;
+
+    r = Math.round(Math.random() *255);
+    g = Math.round(Math.random() *255);
+    b = Math.round(Math.random() *255);
+
+    //Adicionando um atributo ao elemeno button
+    // this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
+    this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
+
+ });
+
+
+//DESAFIO
+//Recupere o elemento tit-sec e atrele a ele um evento de mouse a sua escolha.
+// Altere a cor de fundo e a cor do texto quando o evento ocorer.
+
+//const enterh2  = document.getElementById("enter");
+
+//enterh2.addEventListener("click" , function(){
+   
+//   this.setAttribute("style", `background-color:#ff0000;color:#fff;`);
+//});
+const h2enter = document.querySelector("h2");
+
+h2enter.addEventListener("click", function(){
+    this.setAttribute("style", `background-color:#ff0000;color:#fff;`);
+});
