49f471d3e6c9d9fdc21968a1bb20cf4e89055e7f
AlleSilvaa <126684613+AlleSilvaa@users.noreply.github.com>
2023-05-03 11:06:14 -0300

    atualização

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//DIFERENÇA ENTRE LET/VAR/CONST
+// var nome = "Kayque";
+// if(nome == "Kayque"){
+//     let nome = "Denise";
+// }
+// console.log(nome);
+
+ //Recuperando um elemento button que está no index.html
+ const btn  = 
